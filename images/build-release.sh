b16bf12d92c1ed9c2028436aabcd1ffe9f5a18be
Mads H. Danquah <mads@danquah.dk>
2019-01-15 15:49:24 +0100

    Log when we copy the source-release.
    Also, don't use the "copy if volume is empty" behaviour baked into Docker
    locally, do it with the same script we use in kubernetes instead.
    
    REYOS2-5

--- images/build-release.sh
+++ images/build-release.sh
@@ -3,6 +3,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "${SCRIPT_DIR}"
+source "_versions.source"
 
 if [[ $# -eq 0 ]] ; then
     echo "Syntax: $0 <tag>"
@@ -17,7 +19,8 @@ docker build \
  -t "kkos2display/admin-release:latest" \
  --no-cache \
  -f admin-release/Dockerfile \
+ --build-arg os2display_image_repository="${KKOS_IMAGE_REPOSITORY}" \
  --build-arg revision="${TAG}" \
- --build-arg repository=https://github.com/kkos2/os2display-admin.git \
+ --build-arg repository="${ADMIN_REPOSITORY}" \
  admin-release
 
