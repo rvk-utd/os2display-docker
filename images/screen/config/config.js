f238ff01930c282e1d69b9ec1682a98ab5a09f50
Camilla Krag Jensen <ckj@reload.dk>
2019-01-21 18:34:07 -0600

    Use a more specific BBS name in the docker url

--- images/screen/config/config.js
+++ images/screen/config/config.js
@@ -4,12 +4,12 @@
 window.config = {
   // Used to activate screen (rest API) and load resources.
   "resource": {
-    "server": "//screen.os2display.docker/",
+    "server": "//screen.bbs-os2display.docker/",
     "uri": 'proxy'
   },
   // Used by web-socket.
   "ws": {
-    "server": "//screen.os2display.docker/"
+    "server": "//screen.bbs-os2display.docker/"
   },
   // API key to use.
   "apikey": "059d9d9c50e0c45b529407b183b6a02f",
