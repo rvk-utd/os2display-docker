2837c423066f027d8f518b6dc9e964ca3a7969d0
Mads H. Danquah <mads@danquah.dk>
2019-02-01 22:46:36 +0100

    Moved all of our specifics back into the manifests

--- provisioning/initial-setup/_settings.sh
+++ provisioning/initial-setup/_settings.sh
@@ -3,15 +3,15 @@
 set -euxo pipefail
 
 # GCloud settings
-PROJECT_ID="[insert-project-id-here]"
-REGION="[insert-region-here]"
-ZONE="[insert-zone-here]"
+PROJECT_ID="os2display-kff"
+REGION="europe-west1"
+ZONE="europe-west1-b"
 ADDRESS_NAME="os2display-ingress-ip"
 # Cluster settings
-CLUSTER_NAME="[insert-cluster-name-here]"
+CLUSTER_NAME="os2kff-cluster-1"
 MACHINE_TYPE="n1-standard-4"
-CLUSTER_VER="[insert-cluster-version-here]"
+CLUSTER_VER="1.11.2-gke.18"
 NUM_NODES=1
 MAINTENANCE="02:30" # UTC
 # Set this after running 01-setup-cluster.sh
-# EXTERNAL_IP="[insert-ip-address-here]"
+EXTERNAL_IP="35.233.40.250"
